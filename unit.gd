09573481e81946a88af2da18ea915705f0c6ee35
lptd99 <lptd99@gmail.com>
2026-01-27 20:41:34 -0300

    a

--- unit.gd
+++ unit.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	# direção: player sobe, enemy desce (ajuste se quiser)
 	var dir := -1.0 if master == Master.PLAYER else 1.0
-	$CharacterBody2D.velocity = Vector2(0, -dir * speed)
+	$CharacterBody2D.velocity = Vector2(0, dir * speed)
 	$CharacterBody2D.move_and_slide()
 	
 func init_unit(new_type, new_master):
